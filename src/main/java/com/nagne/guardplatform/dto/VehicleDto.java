769454cc55ccea5cb7eda1ead22a748339955ac2
오희주 <ohuiju@ohuijuui-MacBookPro.local>
2025-05-08 14:17:56 +0900

    🎉 Initial commit

---
+++ src/main/java/com/nagne/guardplatform/dto/VehicleDto.java
@@ -0,0 +1,11 @@
+package com.nagne.guardplatform.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VehicleDto {
+    private String plateNumber;
+    private boolean registered;
+}
---
+++ src/main/java/com/nagne/guardplatform/entity/Warning.java
@@ -0,0 +1,23 @@
+package com.nagne.guardplatform.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Warning {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String plateNumber;
+
+    private String location;
+
+    private LocalDateTime timestamp;
+}
