931855367ad7501d299b6eb5a7f3af97128cda12
오희주 <ohuiju@ohuijuui-MacBookPro.local>
2025-05-14 23:40:52 +0900

    Fix: remove duplicate package and clean up structure

--- src/main/java/com/nagne/guardplatform/repository/VehicleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nagne.guardplatform.repository;
-
-import com.nagne.guardplatform.entity.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, String> {
-}
